8118361f83268df5f4ffe2f60b897ce8e402a016
Adebayo Samuel <eladebayoor@gmail.com>
2023-02-03 00:23:59 -0500

    [feat]: RedactingFormatter class with format method implemented

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@
 
 import re
 from typing import List
+import logging
 
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
@@ -16,3 +17,21 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub(rf"{entry}=(.*?)\{separator}",
                          f'{entry}={redaction}{separator}', message)
     return message
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Init """
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Filter values in incoming logs """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
