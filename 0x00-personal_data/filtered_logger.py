6b210bd5195a37b725115b23d0c5e83b6a05fe17
Adebayo Samuel <eladebayoor@gmail.com>
2023-02-02 23:41:12 -0500

    [feat]: filter_datum method added

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" 0x05. Personal data
+"""
+
+import re
+from typing import List
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """ Replacing """
+    for entry in fields:
+        message = re.sub(rf"{entry}=(.*?)\{separator}",
+                         f'{entry}={redaction}{separator}', message)
+    return message
