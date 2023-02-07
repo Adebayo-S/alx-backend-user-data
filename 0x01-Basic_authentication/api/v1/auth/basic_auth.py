39e59103e70100e5d2c70af3c77f73ab2d2a3cd6
Adebayo Samuel <eladebayoor@gmail.com>
2023-02-06 21:26:14 -0500

    [feat]: basicAuuth class created

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Representation of BasicAuth class
+"""
+import base64
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAuth class.
+    """
+    pass
