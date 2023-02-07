b1163920c8d1e84cb1707e7c56728db040f0f340
Adebayo Samuel <eladebayoor@gmail.com>
2023-02-06 20:43:17 -0500

    [feat]: auth class that manages API authentification

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" 3. Auth class
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Auth class.
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ require_auth method
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ authorization_header method
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ curent_user method
+        """
+        return None
