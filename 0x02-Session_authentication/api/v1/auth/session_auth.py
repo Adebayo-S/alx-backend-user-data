a1e947d3878e1eca5f69fa74a38e3621b6ba7157
Adebayo Samuel <eladebayoor@gmail.com>
2023-02-09 23:03:21 -0500

    [feat]: create_session method to create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -14,3 +14,14 @@ class SessionAuth(Auth):
     """
     SessionAuth class.
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        create_session.
+        """
+        if not user_id or type(user_id) != str:
+            return
+        session_id = str(uuid4())
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id
