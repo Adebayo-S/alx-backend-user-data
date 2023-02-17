7f2ebc5ef988f639b1f2a39ace5defacba0280e8
Adebayo Samuel <eladebayoor@gmail.com>
2023-02-17 00:37:17 -0500

    [feat]: Auth class with register_user method added

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -15,3 +15,26 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode(), salt)
     return hashed
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register user
+        """
+        if not email or not password:
+            return None
+
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+
+            return user
+        else:
+            raise ValueError("User {} already exists".format(email))
