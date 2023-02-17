07dfe6f422ebd17f195d032e75ce21de859a3ab3
Adebayo Samuel <eladebayoor@gmail.com>
2023-02-17 00:47:32 -0500

    [feat]: users POST route that implements user registration implemented

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -15,3 +15,17 @@ def get_message():
     """Get message
     """
     return jsonify({"message": "Bienvenue"})
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """ Register a user
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError as err:
+        return jsonify({"message": "email already registered"}), 400
+
