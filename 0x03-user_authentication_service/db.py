66851a33c859f760cac8e54a3464d20c58b7bbc9
Adebayo Samuel <eladebayoor@gmail.com>
2023-02-17 03:26:52 -0500

    [fix]: remove redundancy

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -59,7 +59,5 @@ class DB:
 
         user = self.find_user_by(id=user_id)
         for k, v in kwargs.items():
-            if not self.valid_query_args(**kwargs):
-                raise ValueError
             setattr(user, k, v)
         self._session.commit()
