c858b41403565febc015510c997fca49d7e8bf04
Adebayo Samuel <eladebayoor@gmail.com>
2023-02-17 01:07:01 -0500

    [feat]: destroy_session get_reset_password_token and update_password method implemented

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -77,3 +77,35 @@ class Auth:
             except NoResultFound:
                 return None
         return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """ Destroy session
+        """
+        if user_id:
+            self._db.update_user(user_id, session_id=None)
+        return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ Generate reset password token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        else:
+            token = str(uuid.uuid4())
+            self._db.update_user(user.id, reset_token=token)
+            return token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        else:
+            hashed_password = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_password)
+            self._db.update_user(user.id, reset_token=None)
+        return None
